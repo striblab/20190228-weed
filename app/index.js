dc07d11921c05e43b6c6c1a2e20c6985cceb205f
Frey Hargarten <harga012@umn.edu>
2019-01-29 12:44:33 -0600

    updates

--- app/index.js
+++ app/index.js
@@ -105,7 +105,14 @@ $(window).on("load", function() {
             })
             .attr('class', 'state')
             .attr('class', function(d) {
-              return "gray3";
+                if (d.state_postal == "MN" || d.state_postal == "SD" || d.state_postal == "MT" || d.state_postal == "UT" || d.state_postal == "HI" || d.state_postal == "AZ"  || d.state_postal == "NM" || d.state_postal == "OK" || d.state_postal == "MO" || d.state_postal == "AR" || d.state_postal == "LA" || d.state_postal == "FL" || d.state_postal == "OH" || d.state_postal == "WV" || d.state_postal == "PA" || d.state_postal == "NY" || d.state_postal == "NJ" || d.state_postal == "RI" || d.state_postal == "NH") {
+                    console.log("ghit");
+                    return "green5";
+                }
+                else if (d.state_postal == "WA" || d.state_postal == "CA" || d.state_postal == "OR" || d.state_postal == "NV" || d.state_postal == "CO" || d.state_postal == "AK"  || d.state_postal == "MI" || d.state_postal == "VT"  || d.state_postal == "MA"  || d.state_postal == "ME") {
+                    return "green3";
+                }
+               else { return "gray3"; }
             })
             .attr('rx', 0)
             .attr('ry', 0)
@@ -124,7 +131,7 @@ $(window).on("load", function() {
         var text = states.append('text')
             .attr('class', 'state-label')
             .attr('class', function(d) {
-                return d.color;
+                return 'state';
             })
             .attr('dominant-baseline', 'central')
             .attr('x', function(d) {
@@ -141,57 +148,57 @@ $(window).on("load", function() {
                 return d.state_postal;
             });
     },
-    state_pos_co2: [{'state_full':'Alabama','state_postal':'AL','row':5,'column':6,'state_total_old':'32','state_total_new':'25','state_change':'-63%','color':'dq1'},
-        {'state_full':'Alaska','state_postal':'AK','row':6,'column':0,'state_total_old':'0','state_total_new':'0','state_change':'Insufficient data','color':'none'},
-        {'state_full':'Arizona','state_postal':'AZ','row':4,'column':1,'state_total_old':'14','state_total_new':'36','state_change':'+36%','color':'dq7'},
-        {'state_full':'Arkansas','state_postal':'AR','row':4,'column':4,'state_total_old':'36','state_total_new':'43','state_change':'-33%','color':'dq3'},
-        {'state_full':'California','state_postal':'CA','row':3,'column':0,'state_total_old':'358','state_total_new':'267','state_change':'-25%','color':'dq3'},
-        {'state_full':'Colorado','state_postal':'CO','row':3,'column':2,'state_total_old':'124','state_total_new':'93','state_change':'-33%','color':'dq3'},
-        {'state_full':'Connecticut','state_postal':'CT','row':2,'column':9,'state_total_old':'0','state_total_new':'6','state_change':'Insufficient data','color':'none'},
-        {'state_full':'D.C.','state_postal':'DC','row':4,'column':8,'state_total_old':'0','state_total_new':'0','state_change':'Insufficient data','color':'none'},
-        {'state_full':'Delaware','state_postal':'DE','row':3,'column':9,'state_total_old':'3','state_total_new':'3','state_change':'-100%','color':'dq1'},
-        {'state_full':'Florida','state_postal':'FL','row':6,'column':8,'state_total_old':'150','state_total_new':'136','state_change':'-14%','color':'dq4'},
-        {'state_full':'Georgia','state_postal':'GA','row':5,'column':7,'state_total_old':'77','state_total_new':'59','state_change':'-38%','color':'dq3'},
-        {'state_full':'Hawaii','state_postal':'HI','row':6,'column':-1,'state_total_old':'0','state_total_new':'11','state_change':'Insufficient data','color':'none'},
-        {'state_full':'Idaho','state_postal':'ID','row':1,'column':1,'state_total_old':'71','state_total_new':'68','state_change':'-21%','color':'dq3'},
-        {'state_full':'Illinois','state_postal':'IL','row':1,'column':6,'state_total_old':'251','state_total_new':'221','state_change':'-14%','color':'dq4'},
-        {'state_full':'Indiana','state_postal':'IN','row':2,'column':5,'state_total_old':'216','state_total_new':'220','state_change':'0%','color':'mid'},
-        {'state_full':'Iowa','state_postal':'IA','row':2,'column':4,'state_total_old':'228','state_total_new':'295','state_change':'+29%','color':'dq6'},
-        {'state_full':'Kansas','state_postal':'KS','row':4,'column':3,'state_total_old':'215','state_total_new':'205','state_change':'-6%','color':'dq4'},
-        {'state_full':'Kentucky','state_postal':'KY','row':3,'column':5,'state_total_old':'308','state_total_new':'162','state_change':'-49%','color':'dq2'},
-        {'state_full':'Louisiana','state_postal':'LA','row':5,'column':4,'state_total_old':'30','state_total_new':'26','state_change':'-53%','color':'dq1'},
-        {'state_full':'Maine','state_postal':'ME','row':-1,'column':10,'state_total_old':'0','state_total_new':'14','state_change':'Insufficient data','color':'none'},
-        {'state_full':'Maryland','state_postal':'MD','row':3,'column':8,'state_total_old':'26','state_total_new':'27','state_change':'-46%','color':'dq2'},
-        {'state_full':'Massachusetts','state_postal':'MA','row':1,'column':9,'state_total_old':'0','state_total_new':'11','state_change':'Insufficient data','color':'none'},
-        {'state_full':'Michigan','state_postal':'MI','row':1,'column':7,'state_total_old':'102','state_total_new':'179','state_change':'+74%','color':'dq8'},
-        {'state_full':'Minnesota','state_postal':'MN','row':1,'column':4,'state_total_old':'153','state_total_new':'210','state_change':'+37%','color':'dq7'},
-        {'state_full':'Mississippi','state_postal':'MS','row':5,'column':5,'state_total_old':'65','state_total_new':'46','state_change':'-57%','color':'dq1'},
-        {'state_full':'Missouri','state_postal':'MO','row':3,'column':4,'state_total_old':'264','state_total_new':'288','state_change':'+5%','color':'dq5'},
-        {'state_full':'Montana','state_postal':'MT','row':1,'column':2,'state_total_old':'145','state_total_new':'139','state_change':'-10%','color':'dq4'},
-        {'state_full':'Nebraska','state_postal':'NE','row':3,'column':3,'state_total_old':'199','state_total_new':'179','state_change':'-11%','color':'dq4'},
-        {'state_full':'Nevada','state_postal':'NV','row':2,'column':1,'state_total_old':'0','state_total_new':'11','state_change':'Insufficient data','color':'none'},
-        {'state_full':'New Hampshire','state_postal':'NH','row':0,'column':10,'state_total_old':'0','state_total_new':'4','state_change':'Insufficient data','color':'none'},
-        {'state_full':'New Jersey','state_postal':'NJ','row':2,'column':8,'state_total_old':'8','state_total_new':'19','state_change':'+50%','color':'dq8'},
-        {'state_full':'New Mexico','state_postal':'NM','row':4,'column':2,'state_total_old':'24','state_total_new':'14','state_change':'-100%','color':'dq1'},
-        {'state_full':'New York','state_postal':'NY','row':1,'column':8,'state_total_old':'203','state_total_new':'135','state_change':'-38%','color':'dq3'},
-        {'state_full':'North Carolina','state_postal':'NC','row':4,'column':6,'state_total_old':'156','state_total_new':'111','state_change':'-35%','color':'dq3'},
-        {'state_full':'North Dakota','state_postal':'ND','row':1,'column':3,'state_total_old':'84','state_total_new':'117','state_change':'+36%','color':'dq7'},
-        {'state_full':'Ohio','state_postal':'OH','row':2,'column':6,'state_total_old':'258','state_total_new':'217','state_change':'-18%','color':'dq4'},
-        {'state_full':'Oklahoma','state_postal':'OK','row':5,'column':3,'state_total_old':'46','state_total_new':'53','state_change':'-30%','color':'dq3'},
-        {'state_full':'Oregon','state_postal':'OR','row':2,'column':0,'state_total_old':'40','state_total_new':'61','state_change':'+20%','color':'dq6'},
-        {'state_full':'Pennsylvania','state_postal':'PA','row':2,'column':7,'state_total_old':'276','state_total_new':'182','state_change':'-34%','color':'dq3'},
-        {'state_full':'Rhode Island','state_postal':'RI','row':2,'column':10,'state_total_old':'0','state_total_new':'0','state_change':'Insufficient data','color':'none'},
-        {'state_full':'South Carolina','state_postal':'SC','row':4,'column':7,'state_total_old':'16','state_total_new':'30','state_change':'+38%','color':'dq7'},
-        {'state_full':'South Dakota','state_postal':'SD','row':2,'column':3,'state_total_old':'88','state_total_new':'108','state_change':'+17%','color':'dq6'},
-        {'state_full':'Tennessee','state_postal':'TN','row':4,'column':5,'state_total_old':'244','state_total_new':'147','state_change':'-42%','color':'dq2'},
-        {'state_full':'Texas','state_postal':'TX','row':6,'column':3,'state_total_old':'223','state_total_new':'159','state_change':'+29%','color':'dq3'},
-        {'state_full':'Utah','state_postal':'UT','row':3,'column':1,'state_total_old':'24','state_total_new':'24','state_change':'-67%','color':'dq1'},
-        {'state_full':'Vermont','state_postal':'VT','row':0,'column':9,'state_total_old':'7','state_total_new':'15','state_change':'-57%','color':'dq1'},
-        {'state_full':'Virginia','state_postal':'VA','row':3,'column':7,'state_total_old':'130','state_total_new':'123','state_change':'-10%','color':'dq4'},
-        {'state_full':'Washington','state_postal':'WA','row':1,'column':0,'state_total_old':'88','state_total_new':'63','state_change':'-39%','color':'dq3'},
-        {'state_full':'West Virginia','state_postal':'WV','row':3,'column':6,'state_total_old':'9','state_total_new':'22','state_change':'0%','color':'mid'},
-        {'state_full':'Wisconsin','state_postal':'WI','row':1,'column':5,'state_total_old':'279','state_total_new':'242','state_change':'-15%','color':'dq4'},
-        {'state_full':'Wyoming','state_postal':'WY','row':2,'column':2,'state_total_old':'33','state_total_new':'37','state_change':'-21%','color':'dq3'}]
+    state_pos_co2: [{'state_full':'Alabama','state_postal':'AL','row':5,'column':6},
+    {'state_full':'Alaska','state_postal':'AK','row':6,'column':0},
+    {'state_full':'Arizona','state_postal':'AZ','row':4,'column':1},
+    {'state_full':'Arkansas','state_postal':'AR','row':4,'column':4},
+    {'state_full':'California','state_postal':'CA','row':3,'column':0},
+    {'state_full':'Colorado','state_postal':'CO','row':3,'column':2},
+    {'state_full':'Connecticut','state_postal':'CT','row':2,'column':9},
+    {'state_full':'D.C.','state_postal':'DC','row':4,'column':8},
+    {'state_full':'Delaware','state_postal':'DE','row':3,'column':9},
+    {'state_full':'Florida','state_postal':'FL','row':6,'column':8},
+    {'state_full':'Georgia','state_postal':'GA','row':5,'column':7},
+    {'state_full':'Hawaii','state_postal':'HI','row':6,'column':-1},
+    {'state_full':'Idaho','state_postal':'ID','row':1,'column':1},
+    {'state_full':'Illinois','state_postal':'IL','row':1,'column':6},
+    {'state_full':'Indiana','state_postal':'IN','row':2,'column':5},
+    {'state_full':'Iowa','state_postal':'IA','row':2,'column':4},
+    {'state_full':'Kansas','state_postal':'KS','row':4,'column':3},
+    {'state_full':'Kentucky','state_postal':'KY','row':3,'column':5},
+    {'state_full':'Louisiana','state_postal':'LA','row':5,'column':4},
+    {'state_full':'Maine','state_postal':'ME','row':-1,'column':10},
+    {'state_full':'Maryland','state_postal':'MD','row':3,'column':8},
+    {'state_full':'Massachusetts','state_postal':'MA','row':1,'column':9},
+    {'state_full':'Michigan','state_postal':'MI','row':1,'column':7},
+    {'state_full':'Minnesota','state_postal':'MN','row':1,'column':4},
+    {'state_full':'Mississippi','state_postal':'MS','row':5,'column':5},
+    {'state_full':'Missouri','state_postal':'MO','row':3,'column':4},
+    {'state_full':'Montana','state_postal':'MT','row':1,'column':2},
+    {'state_full':'Nebraska','state_postal':'NE','row':3,'column':3},
+    {'state_full':'Nevada','state_postal':'NV','row':2,'column':1},
+    {'state_full':'New Hampshire','state_postal':'NH','row':0,'column':10},
+    {'state_full':'New Jersey','state_postal':'NJ','row':2,'column':8,},
+    {'state_full':'New Mexico','state_postal':'NM','row':4,'column':2},
+    {'state_full':'New York','state_postal':'NY','row':1,'column':8,},
+    {'state_full':'North Carolina','state_postal':'NC','row':4,'column':6},
+    {'state_full':'North Dakota','state_postal':'ND','row':1,'column':3},
+    {'state_full':'Ohio','state_postal':'OH','row':2,'column':6},
+    {'state_full':'Oklahoma','state_postal':'OK','row':5,'column':3},
+    {'state_full':'Oregon','state_postal':'OR','row':2,'column':0},
+    {'state_full':'Pennsylvania','state_postal':'PA','row':2,'column':7},
+    {'state_full':'Rhode Island','state_postal':'RI','row':2,'column':10},
+    {'state_full':'South Carolina','state_postal':'SC','row':4,'column':7},
+    {'state_full':'South Dakota','state_postal':'SD','row':2,'column':3},
+    {'state_full':'Tennessee','state_postal':'TN','row':4,'column':5},
+    {'state_full':'Texas','state_postal':'TX','row':6,'column':3},
+    {'state_full':'Utah','state_postal':'UT','row':3,'column':1},
+    {'state_full':'Vermont','state_postal':'VT','row':0,'column':9},
+    {'state_full':'Virginia','state_postal':'VA','row':3,'column':7},
+    {'state_full':'Washington','state_postal':'WA','row':1,'column':0},
+    {'state_full':'West Virginia','state_postal':'WV','row':3,'column':6},
+    {'state_full':'Wisconsin','state_postal':'WI','row':1,'column':5},
+    {'state_full':'Wyoming','state_postal':'WY','row':2,'column':2}]
 };
 
   cartogram1.init();
