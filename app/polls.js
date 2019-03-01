27cc7b69b524c1642ce4b7ee950d75b662144bdd
Frey Hargarten <harga012@umn.edu>
2019-03-01 15:18:32 -0600

    updates

--- app/polls.js
+++ app/polls.js
@@ -24,9 +24,9 @@ class PollChart {
             data: {
                 x: 'x',
                 columns: [
-                    ['x',2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019],
-                    ['US',0.31,null,null,0.34,null,0.36,null,null,null,null,0.46,null,0.48,0.58,0.51,0.58,null,0.64,0.66,null],
-                    ['Midwest',null,null,null,null,null,null,null,null,null,0.4,0.41,0.54,0.44,0.58,0.45,null,null,null,0.65,null]
+                    ['x',1970,1971,1972,1973,1974,1975,1976,1977,1978,1979,1980,1981,1982,1983,1984,1985,1986,1987,1988,1989,1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019],
+                    ['US',0.12,null,null,0.16,null,null,null,0.28,null,null,0.25,null,null,null,null,0.23,null,null,null,null,null,null,null,null,0.25,null,null,null,null,null,0.31,null,null,0.34,null,0.36,null,null,null,null,0.46,null,0.48,0.58,0.51,0.58,null,0.64,0.66,null],
+                    ['Midwest',null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,0.4,0.41,0.54,0.44,0.58,0.45,null,null,null,0.65,null]
                 ],
                 type: 'line',
                 labels: {
